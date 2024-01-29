79e878d848535bf536eb71b1829fda6bc69b5b45
tomwilsonsco <thomaswilson81@gmail.com>
2024-01-29 11:22:26 +0000

    reset demo changes

--- functions/my_check.R
+++
@@ -1,6 +0,0 @@
-my_check <- function(x){
-  # this is a test comment I have added
-  if (! is.numeric(class)){
-    stop("input must be numeric")
-  }
-}
--- functions/my_sum.R
+++ functions/my_sum.R
@@ -1,5 +1,3 @@
-source(my_check.R)
 my_sum <- function(x) {
-  my_check(x)
   sum(x)
 }
