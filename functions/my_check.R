5bcb467b0cbc5c1d70854715dad7f229a6822b02
tomwilsonsco <thomaswilson81@gmail.com>
2024-01-29 11:12:03 +0000

    new method

--- functions/my_check.R
+++ functions/my_check.R
@@ -1,6 +1,7 @@
 my_check <- function(x){
   # this is a test comment I have added
-  if (! class(x) == "numeric"){
+  # a better method
+  if (! is.numeric(class)){
     stop("input must be numeric")
   }
 }
